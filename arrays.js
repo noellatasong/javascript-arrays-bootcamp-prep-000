34062af89684116963df4d408fde3ca39aaf3f9a
noella tasong <ntemgwanoella@gmail.com>
2018-07-22 20:40:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,11 +3,14 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray() {
   var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
   chocolateBars = ['mars', ...chocolateBars] 
+  // return array here ['mars','snickers',' hundred grand', 'kitkat', 'skittles']
+  chocolateBars // ['snickers', 'hundred grand', 'kitkat', 'skittles']
 }
 
 function destructivelyAddElementToBeginningOfArray() {
   var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
   chocolateBars.unshift('mars');
+  // return array here ['mars', 'snickers', 'hundred grand', 'kitkat', 'skittles']
 }
 
 function addElementToEndOfArray() {
