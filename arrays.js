dae7c9a59443b2ae12f1a799a077ebe28775325d
noella tasong <ntemgwanoella@gmail.com>
2018-07-22 20:47:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray() {
   var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
   chocolateBars = ['mars', ...chocolateBars] 
-  // return array here ['mars','snickers',' hundred grand', 'kitkat', 'skittles']
+  //return array here ['mars','snickers',' hundred grand', 'kitkat', 'skittles']
   chocolateBars // ['snickers', 'hundred grand', 'kitkat', 'skittles']
 }
 
